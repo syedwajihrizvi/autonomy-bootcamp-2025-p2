3386bed4380f2161c3640d9f06c8349ab0b41c83
AB Rizvi <abrizvi@ABs-MacBook-Pro.local>
2026-05-19 17:01:33 -0400

    Removed unused import

--- bootcamp_main.py
+++ bootcamp_main.py
@@ -5,7 +5,6 @@ Main process to setup and manage all the other working processes
 """
 
 import multiprocessing as mp
-import queue
 import time
 
 from pymavlink import mavutil
@@ -150,7 +149,7 @@ def main() -> int:
         local_logger=main_logger,
     )
     # Create the workers (processes) and obtain their managers
-    worker_managers = list[worker_manager.WorkerManager] = []
+    worker_managers =[]
     result, heartbeat_sender_manager = worker_manager.WorkerManager.create(
         worker_properties=heartbeat_sender_worker_properties, local_logger=main_logger
     )
